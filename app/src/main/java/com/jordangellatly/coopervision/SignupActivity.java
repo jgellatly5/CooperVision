7ce6975de1c3b3ce72d3155956026207a50a12b4
Jordan Gellatly <jgellatly5@gmail.com>
2018-06-23 22:47:32 -0700

    Basic login authentication Login has been tested for first user added to authetication in firebase

--- app/src/main/java/com/jordangellatly/coopervision/SignupActivity.java
+++ app/src/main/java/com/jordangellatly/coopervision/SignupActivity.java
@@ -1,8 +1,8 @@
 package com.jordangellatly.coopervision;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +16,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
 
@@ -45,18 +45,18 @@ public class LoginActivity extends AppCompatActivity {
                 String email = mEmail.getText().toString();
                 String password = mPassword.getText().toString();
 
-                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
                             Log.d(TAG, "onComplete: " + user.getDisplayName());
-                            Toast.makeText(LoginActivity.this, "You have signed in", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignupActivity.this, "You have signed in", Toast.LENGTH_SHORT).show();
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignupActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
