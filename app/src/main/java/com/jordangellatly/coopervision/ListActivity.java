2d067a9501386c0692bacb46d6cdbbf0ab80c60b
Jordan Gellatly <jgellatly5@gmail.com>
2018-06-26 21:39:19 -0700

    Added search functionality The app bar now has the ability to search or filter chemicals by typing in name

--- app/src/main/java/com/jordangellatly/coopervision/ListActivity.java
+++ app/src/main/java/com/jordangellatly/coopervision/ListActivity.java
@@ -1,14 +1,23 @@
 package com.jordangellatly.coopervision;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
 import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
@@ -28,6 +37,8 @@ public class ListActivity extends AppCompatActivity {
     private DatabaseReference myRef;
 
     private ListView listView;
+    private Toolbar tbMainSearch;
+    private ArrayAdapter<String> itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +46,10 @@ public class ListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
 
         listView = findViewById(R.id.list_view);
+        tbMainSearch = findViewById(R.id.toolbar_search);
+        setSupportActionBar(tbMainSearch);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("masterSheet");
@@ -51,7 +66,7 @@ public class ListActivity extends AppCompatActivity {
                         chemicals.add(chemical);
                     }
                 }
-                ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(ListActivity.this, android.R.layout.simple_list_item_1, chemicals);
+                itemsAdapter = new ArrayAdapter<String>(ListActivity.this, android.R.layout.simple_list_item_1, chemicals);
                 listView.setAdapter(itemsAdapter);
 
             }
@@ -76,5 +91,31 @@ public class ListActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_search, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        MenuItem mSearchMenuItem = menu.findItem(R.id.menu_toolbarsearch);
+        SearchView searchView = (SearchView) mSearchMenuItem.getActionView();
+        searchView.setQueryHint("Enter Text");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Log.d(TAG, "onQueryTextSubmit: query: " + s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                Log.d(TAG, "onQueryTextChange: newText: " + s);
+                itemsAdapter.getFilter().filter(s);
+                return true;
+            }
+        });
+        Log.d(TAG, "onCreateOptionsMenu: mMenuSearchItem: " + mSearchMenuItem.getActionView());
+        return true;
     }
 }
