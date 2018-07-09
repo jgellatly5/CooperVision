12e7d989f5beacee3a55f6a20bd135dc35a7d57a
Jordan Gellatly <jgellatly5@gmail.com>
2018-07-08 21:00:18 -0700

    Updated Detail view to display data in table The text for the chemical name continues to overflow. Need to dynamically autosize

--- app/src/main/java/com/jordangellatly/coopervision/DetailActivity.java
+++ app/src/main/java/com/jordangellatly/coopervision/DetailActivity.java
@@ -3,7 +3,9 @@ package com.jordangellatly.coopervision;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.parceler.Parcels;
 
@@ -21,30 +23,59 @@ public class DetailActivity extends AppCompatActivity {
     private TextView tvManufacturer;
     private TextView tvType;
 
+    private TextView btnEdit;
+    private TextView btnDelete;
+    private TextView btnRequestPurchase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        tvLocation = findViewById(R.id.tv_location);
+        tvLocation = findViewById(R.id.tv_location_value);
         tvName = findViewById(R.id.tv_name);
-        tvRecDate = findViewById(R.id.tv_rec_date);
-        tvExpDate = findViewById(R.id.tv_exp_date);
-        tvLotOrder = findViewById(R.id.tv_lot_order);
-        tvBottleCount = findViewById(R.id.tv_bottle_count);
-        tvCasNumber = findViewById(R.id.tv_cas_number);
-        tvManufacturer = findViewById(R.id.tv_manufacturer);
-        tvType = findViewById(R.id.tv_type);
+        tvRecDate = findViewById(R.id.tv_rec_date_value);
+        tvExpDate = findViewById(R.id.tv_exp_date_value);
+        tvLotOrder = findViewById(R.id.tv_lot_order_value);
+        tvBottleCount = findViewById(R.id.tv_bottle_count_value);
+        tvCasNumber = findViewById(R.id.tv_cas_number_value);
+        tvManufacturer = findViewById(R.id.tv_manufacturer_value);
+        tvType = findViewById(R.id.tv_type_value);
+
+        btnEdit = findViewById(R.id.btn_edit);
+        btnDelete = findViewById(R.id.btn_delete);
+        btnRequestPurchase = findViewById(R.id.btn_request);
 
         Chemicals chemicalFromIntent = Parcels.unwrap(getIntent().getParcelableExtra("chemical"));
-        tvName.setText(getResources().getString(R.string.chemical_name) + chemicalFromIntent.getMaterialName());
-        tvLocation.setText(getResources().getString(R.string.chemical_location) + chemicalFromIntent.getLocationInLab());
-        tvRecDate.setText(getResources().getString(R.string.received_date) + chemicalFromIntent.getReceiveDate());
-        tvExpDate.setText(getResources().getString(R.string.exp_date) + chemicalFromIntent.getExpirationDate());
-        tvLotOrder.setText(getResources().getString(R.string.lot_order_number) + chemicalFromIntent.getLotOrderNumber());
-        tvBottleCount.setText(getResources().getString(R.string.bottle_count) + chemicalFromIntent.getBottleCount().toString());
-        tvCasNumber.setText(getResources().getString(R.string.cas_number) + chemicalFromIntent.getCasNumber());
-        tvManufacturer.setText(getResources().getString(R.string.manufacturer) + chemicalFromIntent.getManufacturer());
-        tvType.setText(getResources().getString(R.string.type) + chemicalFromIntent.getType());
+        tvName.setText(chemicalFromIntent.getMaterialName());
+        tvLocation.setText(chemicalFromIntent.getLocationInLab());
+        tvRecDate.setText(chemicalFromIntent.getReceiveDate());
+        tvExpDate.setText(chemicalFromIntent.getExpirationDate());
+        tvLotOrder.setText(chemicalFromIntent.getLotOrderNumber());
+        tvBottleCount.setText(chemicalFromIntent.getBottleCount().toString());
+        tvCasNumber.setText(chemicalFromIntent.getCasNumber());
+        tvManufacturer.setText(chemicalFromIntent.getManufacturer());
+        tvType.setText(chemicalFromIntent.getType());
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DetailActivity.this, "Edit this item.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DetailActivity.this, "Delete this item.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnRequestPurchase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DetailActivity.this, "Request purchase order.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
