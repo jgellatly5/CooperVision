86ecdb38ad9ce7ddb85178eb1a3db0df0ca70786
jgellatly5 <jgellatly5@gmail.com>
2018-11-22 10:26:01 -0800

    converting signup to kotlin

--- app/src/main/java/com/jordangellatly/coopervision/auth/LoginActivity.kt
+++ app/src/main/java/com/jordangellatly/coopervision/auth/LoginActivity.kt
@@ -4,24 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.jordangellatly.coopervision.search.FilterActivity
 import com.jordangellatly.coopervision.R
-
-import butterknife.BindView
-import butterknife.ButterKnife
-import butterknife.OnClick
-import de.hdodenhof.circleimageview.CircleImageView
-
+import com.jordangellatly.coopervision.search.FilterActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -32,7 +18,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        ButterKnife.bind(this)
 
         mAuth = FirebaseAuth.getInstance()
 
---
+++ app/src/main/java/com/jordangellatly/coopervision/auth/SignupActivity.kt
@@ -0,0 +1,45 @@
+package com.jordangellatly.coopervision.auth
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.jordangellatly.coopervision.R
+import kotlinx.android.synthetic.main.activity_signup.*
+
+class SignupActivity : AppCompatActivity() {
+
+    private val TAG = "SignupActivity"
+    private var mAuth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        btn_signup.setOnClickListener { v -> signUp() }
+        tv_switch_login.setOnClickListener { v ->
+            val intent = Intent(this@SignupActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun signUp() {
+        val email = et_email.text.toString()
+        val password = et_password.text.toString()
+
+        mAuth!!.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this@SignupActivity) { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this@SignupActivity, "You have created a new account. Please sign in.", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@SignupActivity, LoginActivity::class.java)
+                startActivity(intent)
+            } else {
+                Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                Toast.makeText(this@SignupActivity, "Could not create user.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
